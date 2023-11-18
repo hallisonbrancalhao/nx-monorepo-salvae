99c075b46e444abf616550c0de690944a35c2985
Hallison Brancalhao <ra-20055608-2@alunos.unicesumar.edu.br>
2023-11-18 18:11:21 -0300

    feat(libs): create base libs

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'feature',
+    loadChildren: () =>
+      import('@nx-monorepo-salvae/feature').then((m) => m.featureRoutes),
+  },
+];
---
+++ libs/feature/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { FeatureComponent } from './feature/feature.component';
+
+export const featureRoutes: Route[] = [
+  { path: '', component: FeatureComponent },
+];
